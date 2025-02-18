1fcca1622349edaddd0b7137e21ffc34cc5da628
wx-ecology <wenjing.xuuu@gmail.com>
2025-02-18 12:36:35 +0100

    update to the new spatial autocorrelation coding

--- code/3_modeling_individual vs species .R
+++ code/3_modeling_individual vs species .R
@@ -13,8 +13,8 @@ library(ggplot2)
 ###################################################
 # ----- read data and prep data for modeling ------ 
 ###################################################
-target_time_scale_days = 10
-tartget_spatial_scale = "me" 
+target_time_scale_days = 1
+tartget_spatial_scale = "hi" 
 
 move_covid <- read_rds( paste0("./data/movement/ready_data/move", target_time_scale_days,"d_covid.rds"))
 move_movebank <- read_rds(paste0("./data/movement/ready_data/move", target_time_scale_days,"d_movebank.rds"))
@@ -96,7 +96,7 @@ if (any(duplicated(c(move.sum$Longitude, move.sum$Latitude)))) {
 table(move.sum$Binomial)
 move.sum <- move.sum %>% filter(Binomial %in% (move.sum %>% group_by(Binomial) %>% 
                                                  summarize (n = length(unique(ID))) %>% 
-                                                 filter(n >= 3) %>% pull(Binomial))) # 39 -- > 26 spp for 10 d, 19 for 1d
+                                                 filter(n >= 5) %>% pull(Binomial))) # 39 -- > 26 spp for 10 d, 19 for 1d
 length(unique(move.sum$Binomial))
 nrow(move.sum) # 2044 or 625 for 1day
 
@@ -104,11 +104,13 @@ nrow(move.sum) # 2044 or 625 for 1day
 move.sum <- move.sum %>%
   mutate(log_BodyMass_kg = log(BodyMass_kg), 
          scale_NDVI = scale(NDVI),
-         log_HFI = ifelse(HFI==0, log(HFI+0.001), log(HFI)),
-         log_HMI = ifelse(HMI==0, log(HMI+0.001), log(HMI)),
+          # log_HFI = ifelse(HFI==0, log(HFI+0.001), log(HFI)),
+          # log_HMI = ifelse(HMI==0, log(HMI+0.001), log(HMI)),
+         scale_HFI = scale(HFI),
+         scale_HMI = scale(HMI, center = TRUE, scale = FALSE),
          log_dist_2_build = log(dist_2_build),
          log_pd = log(pd_adpt_km),
-         log_bd = ifelse(bd_adpt==0, log(bd_adpt+0.001), log(bd_adpt)),
+         slog_bd = scale(ifelse(bd_adpt==0, log(bd_adpt+0.001), log(bd_adpt))),
          log_Displacement_km = log(Displacement_km)) %>%
   rename(lon = Longitude,
          lat = Latitude)
@@ -122,9 +124,9 @@ colSums(is.na(move.sum))
 sapply(move.sum, function(col) sum(is.nan(col)))
 sapply(move.sum, function(col) sum(is.infinite(col)))
 
-write_rds(move.sum, paste0("./data/movement/ready_data/move_",target_time_scale_days, "d_",
-                           tartget_spatial_scale,
-                           ".rds"))
+# write_rds(move.sum, paste0("./data/movement/ready_data/move_",target_time_scale_days, "d_",
+#                            tartget_spatial_scale,
+#                            ".rds"))
 
 ###################################################
 # -------------------- run models -----------------
@@ -137,38 +139,64 @@ target_time_scale_days
 tartget_spatial_scale 
 
 results = tibble()
-for (i in c("log_HFI", "log_HMI", "log_bd") ) {
+mR2 = tibble()
+models = tibble()
+for (i in c(
+  "scale_HFI", "scale_HMI", "slog_bd"
+) ) {
   
-  if (i == "log_bd") {
-    formula <- as.formula(paste0("log_Displacement_km ~ log_BodyMass_kg + scale_NDVI + Diet + ", i, " + log_dist_2_build + log_pd_spp + log_pd_ind"))
-  } else {
-    formula <- as.formula(paste0("log_Displacement_km ~ log_BodyMass_kg + scale_NDVI + Diet + ", i, " + log_pd_spp + log_pd_ind"))
-  }
-  
-  print (paste0("calculating for ", i, " SPP vs IND model..."))
-  m = lme(formula,
-           correlation = corHaversine(form=~lon+lat, mimic = "corGaus"),
+  if (i == "scale_HFI") {
+    print (paste0("calculating ", i, " SPP vs IND model..."))
+    m = lme(log_Displacement_km ~ log_BodyMass_kg + scale_NDVI + Diet + scale_HFI + log_pd_spp + log_pd_ind,
+            correlation = corSpatial(form=~lon+lat, type = "exponential"),
            random= ~1|Order/Family/Genus/Species,
            control = list(opt = "optim", msMaxIter = 1000, msMaxEval = 1000), method = "ML",
            data = dat)
-  saveRDS(m, paste0("./results/models/sppVSind_",target_time_scale_days, "d_",tartget_spatial_scale, "_", i, ".rds"))
   
+    print (paste0("calculating mR2 for the ", i, " pd model..."))
+    mR2.i <- glmm.hp(m)
+  } else if (i == "scale_HMI") {
+    print (paste0("calculating ", i, " SPP vs IND model..."))
+    m = lme(log_Displacement_km ~ log_BodyMass_kg + scale_NDVI + Diet + scale_HMI + log_pd_spp + log_pd_ind,
+            correlation = corSpatial(form=~lon+lat, type = "exponential"),
+            random= ~1|Order/Family/Genus/Species,
+            control = list(opt = "optim", msMaxIter = 1000, msMaxEval = 1000), method = "ML",
+            data = dat)
+    
+    print (paste0("calculating mR2 for the ", i, " pd model..."))
+    mR2.i <- glmm.hp(m)
+  } else if (i == "slog_bd") {
+    print (paste0("calculating ", i, " SPP vs IND model..."))
+    m = lme(log_Displacement_km ~ log_BodyMass_kg + scale_NDVI + Diet + slog_bd + log_dist_2_build + log_pd_spp + log_pd_ind,
+            correlation = corSpatial(form=~lon+lat, type = "exponential"),
+            random= ~1|Order/Family/Genus/Species,
+            control = list(opt = "optim", msMaxIter = 1000, msMaxEval = 1000), method = "ML",
+            data = dat)
+    
+    print (paste0("calculating mR2 for the ", i, " pd model..."))
+    mR2.i <- glmm.hp(m)
+  }
   
-  sum.i = tibble(temporal_scale = paste0(target_time_scale_days, "d"),
+  models <- rbind(models, tibble_row(comp_var = i, conf_var = "log_pd", model = m, mR2 = mR2.i ))
+ 
+  sum = tibble(temporal_scale = paste0(target_time_scale_days, "d"),
                  spatial_scale = paste0("disp_", tartget_spatial_scale),
                  amount_variable = i,
                  beta_pd_spp = (summary(m))$tTable["log_pd_spp", "Value"],
                  upper_pd_spp = intervals(m, which = "fixed")$fixed["log_pd_spp","upper"],
                  lower_pd_spp = intervals(m, which = "fixed")$fixed["log_pd_spp","lower"],
                  p_value_pd_spp = (summary(m))$tTable["log_pd_spp", "p-value"],
+                 mR2_pd_spp = mR2.i$hierarchical.partitioning["log_pd_spp","I.perc(%)"],
                  beta_pd_ind = (summary(m))$tTable["log_pd_ind", "Value"],
                  upper_pd_ind = intervals(m, which = "fixed")$fixed["log_pd_ind","upper"],
                  lower_pd_ind = intervals(m, which = "fixed")$fixed["log_pd_ind","lower"],
-                 p_value_pd_ind = (summary(m))$tTable["log_pd_ind", "p-value"])
+                 p_value_pd_ind = (summary(m))$tTable["log_pd_ind", "p-value"],
+                 mR2_pd_ind = mR2.i$hierarchical.partitioning["log_pd_ind","I.perc(%)"])
   
-  results = rbind(results, sum.i)
+  results = rbind(results, sum)
 } 
 
+saveRDS(models, paste0("./results/models/zscored/Mods_sppVSind_",target_time_scale_days, "d_",tartget_spatial_scale, ".rds"))
 
-write_csv(results, paste0("./results/ModResults_SPPvsIND_move",target_time_scale_days, "d_",tartget_spatial_scale, ".csv"))
+write_csv(results, paste0("./results/ModResults_SPPvsIND/scale_zscored/ModResults_SPPvsIND_move",target_time_scale_days, "d_",tartget_spatial_scale, ".csv"))
 
